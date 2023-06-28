1ad51c3efde7b4df8b26cf38e800ba495180eb87
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-27 23:31:05 -0400

    Routers

---
+++ src/router.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Produto
+ */
+
+router.get("/product", (req, res) => {
+  res.json({ message: "hello" });
+});
+router.get("/product/:id", (req, res) => { });
+router.post("/product", (req, res) => { });
+router.put("/product/:id", (req, res) => { });
+router.delete("/product/:id", (req, res) => { });
+
+/**
+ * Update
+ */
+
+router.get("/update", () => { });
+router.get("/update/:id", (req, res) => { });
+router.post("/update", (req, res) => { });
+router.put("/update/:id", (req, res) => { });
+router.delete("/update/:id", (req, res) => { });
+
+/**
+ * Update Point
+ */
+
+router.get("/updatepoint", (req, res) => { });
+router.get("/updatepoint/:id", (req, res) => { });
+router.post("/updatepoint", (req, res) => { });
+router.put("/updatepoint/:id", (req, res) => { });
+router.delete("/updatepoint/:id", (req, res) => { });
+
+export default router;
