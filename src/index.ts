29bd907fb8fb9ea21e40fc79e9233bd18a80f5b1
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-29 00:53:39 -0400

    Creating JWT

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import * as dotenv from "dotenv";
 import app from "./server";
+
+dotenv.config();
 
 app.listen(3009, () => {
   console.log("Server is listening http://localhost:3009");
---
+++ src/modules/auth.ts
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.name },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
